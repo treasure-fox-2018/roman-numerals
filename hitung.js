a7eaf4fed8b039477beeaf1c9494d9b96edae9d0
HelmiYogantara <helmiyogantara@gmail.com>
2018-05-28 19:27:15 +0700

    exercise roman-numerals done!

---
+++ hitung.js
@@ -0,0 +1,3 @@
+console.log(4 % 5)
+console.log(5 % 5);
+console.log(6 % 5);
--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,23 @@
 function to_roman (num) {
-  // your implementation code here
+  var object = {  M : 1000,
+                  D : 500,
+                  CD : 400,
+                  C : 100,
+                  L : 50,
+                  XL : 40,
+                  X : 10,
+                  IX : 9,
+                  V : 5,
+                  IV : 4,
+                  I :1}
+  romawi = ''
+  for(var x in object){
+    while(num >= object[x]){
+      romawi += x
+      num -= object[x]
+    }
+  }
+  return romawi
 }
 
 // Drive code
