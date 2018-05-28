66832fca27a6f5a283fce9be6965147310f5cb7b
Richardson12345 <chuddywarrior@gmail.com>
2018-05-28 15:01:05 +0700

    first Commit

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,80 @@
 function to_roman (num) {
-  // your implementation code here
+  let romanNumeral = ['I','V','X','L','C','D','M'];
+  let arabicNumeral = [1,5,10,50,100,500,1000];
+  let stringNum = num.toString();
+  let ones = stringNum.slice(-1);
+  let tens = (stringNum.slice(-2))-(stringNum.slice(-1))
+  let hundreds = (stringNum.slice(-3))-(stringNum.slice(-2));
+  let thousands = (stringNum.slice(-4))-(stringNum.slice(-3));
+  let onesRoman ='';
+  let tensRoman = '';
+  let hundredsRoman = '';
+  let thousandsRoman = '';
+    // return tens
+    while(ones > 0){
+      if(ones === '4'){
+        onesRoman += 'IV';
+        break;
+      }
+      if(ones === '9'){
+        onesRoman += 'IX';
+        break;
+      }
+      for(let z = arabicNumeral.length-1; z >= 0 ; z--){
+        if(ones >= arabicNumeral[z]){
+          onesRoman += romanNumeral[z];
+          ones -= arabicNumeral[z];
+          break
+        }
+      }
+    }
+    while(tens > 0){
+      if(tens === 40){
+        tensRoman += 'XL';
+        break;
+      }
+      if(tens === 90){
+        tensRoman += 'XC';
+        break;
+      }
+      for(let z = arabicNumeral.length-1; z >= 0 ; z--){
+        if(tens >= arabicNumeral[z]){
+          tensRoman += romanNumeral[z];
+          tens -= arabicNumeral[z];
+          break
+        }
+
+      }
+    }
+    while(hundreds > 0){
+      if(hundreds === 400){
+        hundredsRoman += 'DC';
+        break;
+      }
+      if(hundreds === 900){
+        hundredsRoman += 'CM';
+        break;
+      }
+      for(let z = arabicNumeral.length-1; z >= 0 ; z--){
+        if(hundreds >= arabicNumeral[z]){
+          hundredsRoman += romanNumeral[z];
+          hundreds -= arabicNumeral[z];
+          break
+        }
+
+      }
+    }
+    while(thousands > 0){
+      for(let z = arabicNumeral.length-1; z >= 0 ; z--){
+        if(thousands >= arabicNumeral[z]){
+          thousandsRoman += romanNumeral[z];
+          thousands -= arabicNumeral[z];
+          break
+        }
+
+      }
+    }
+    return `${thousandsRoman}${hundredsRoman}${tensRoman}${onesRoman}`
 }
 
 // Drive code
