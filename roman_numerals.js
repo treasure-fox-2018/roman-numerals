64fff471c9e429b6dc0e44b6e0e09b20807e527e
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2018-05-28 19:29:55 +0700

    Tugas roman-numerals Fadli Al Baihaqi done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,4 +1,52 @@
 function to_roman (num) {
+  var hasil = '';
+
+  while (num >= 1000 && num <= 3000) {
+    hasil = hasil + 'M';
+    num = num - 1000;
+  }
+  while (num >= 500) {
+    hasil = hasil + 'C';
+    num = num - 500;
+  }
+  while (num >= 400) {
+    hasil = hasil + 'CD';
+    num = num - 400;
+  }
+  while (num >= 100) {
+    hasil = hasil + 'D';
+    num = num - 100;
+  }
+  while (num >= 50) {
+    hasil = hasil + 'L';
+    num = num - 50;
+  }
+  while (num >= 40) {
+    hasil = hasil + 'XL';
+    num = num - 40;
+  }
+  while (num >= 10) {
+    hasil = hasil + 'X';
+    num = num - 10;
+  }
+  while (num >= 9) {
+    hasil = hasil + 'IX';
+    num = num - 9;
+  }
+  while (num >= 5) {
+    hasil = hasil + 'V';
+    num = num - 5;
+  }
+  while (num >= 4) {
+    hasil = hasil + 'IV';
+    num = num - 4;
+  }
+  while (num >= 1) {
+    hasil = hasil + 'I';
+    num = num - 1;
+  }
+
+  return hasil;
   // your implementation code here
 }
 
