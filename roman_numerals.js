50f6c4a9001d745203c9340ff1deaba17fa1d616
mario <panggihmario@gmail.com>
2018-05-28 22:02:13 +0700

    numeral_roman

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,11 +1,23 @@
 function to_roman (num) {
   // your implementation code here
+var formula = [
+                ["M" ,1000],["CM",900],["D",500],["CD",400],["C",100],["XC",90],["L",50],["XL",40],["X",10],["IX",9],["V",5],["IV",4],["I",1]
+              ]
+  var tempt =''
+  for(let i=0;i<formula.length;i++){
+    while(num >=formula[i][1]){
+      tempt+=formula[i][0]
+      num-=formula[i][1]  
+    }
+  }
+  return tempt
 }
 
 // Drive code
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
 console.log('——————|——————————|———————')
+console.log('5     | IV       | ', to_roman(5))
 console.log('4     | IV       | ', to_roman(4))
 console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
