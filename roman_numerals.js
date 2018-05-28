30ad85cb8d8138873eed1fbcfc3bb60e2f8603a2
Rhesa Utomo <venderblitz@gmail.com>
2018-05-28 12:10:36 +0700

    finished

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,24 @@
-function to_roman (num) {
+function to_roman (value) {
   // your implementation code here
+  const romanDict = { 1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 
+                      40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 
+                      500: 'D', 900: 'CM', 1000: 'M' };
+  const numbers = Object.keys(romanDict); // returns arrays of the keys (which are the numbers)
+  // const romans = Object.values(romanDict)
+  let i = numbers.length - 1;
+  let output = '';
+  
+  while(value !== 0) {
+    let number = numbers[i]
+    if (value - number >= 0) {
+      value -= number;
+      output += romanDict[number];
+    }
+    else {
+      i--;
+    }
+  }
+  return output;
 }
 
 // Drive code
