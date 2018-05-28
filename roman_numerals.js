7986c235d71dae71799038fd077c8edc13ebb604
Ari Supriatna <arisupriatna@fabio-Aspire-E5-476G.localdomain>
2018-05-28 19:00:46 +0700

    roman_numerals done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,20 @@
-function to_roman (num) {
-  // your implementation code here
+function to_roman(num) {
+
+  let romawi = ''
+
+  let roman = { 'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1 }
+
+  for (let key in roman) {
+    while (num >= roman[key]) {
+      if (num >= roman[key]) {
+        num -= roman[key]
+        romawi += key
+      }
+    }
+  }
+
+
+  return romawi
 }
 
 // Drive code
@@ -11,3 +26,5 @@ console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
 console.log('1646  | MDCXLVI  | ', to_roman(1646))
+
+module.exports = to_roman
