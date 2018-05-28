7a6385840986efd5ae211694c2780941180a9421
wisnugautama <ngurahwisnugautama@gmail.com>
2018-05-28 20:36:50 +0700

    exercise roman_numerals done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,28 @@
 function to_roman (num) {
   // your implementation code here
+  var output = '';
+  var romawi = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  }
+  for(var i in romawi){
+    while (num >= romawi[i]) {
+      output += i;
+      num -= romawi[i];
+    }
+  }
+  return output;
 }
 
 // Drive code
