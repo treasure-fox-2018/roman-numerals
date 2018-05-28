2ec5293c3a5993450b6e1562150450266c881e32
andrewsasmita <its.me.andrew.sasmita@gmail.com>
2018-05-28 13:32:15 +0700

    further increased efficiency

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,68 @@
+
+
 function to_roman (num) {
-  // your implementation code here
+
+  let number = num
+  
+  const romanLib = 
+  [
+    {
+      name: 'I',
+      value: 1
+    }, {
+      name: 'IV',
+      value: 4
+    }, {
+      name: 'V',
+      value: 5
+    }, {
+      name: 'IX',
+      value: 9
+    }, {
+      name: 'X',
+      value: 10
+    }, {
+      name: 'XL',
+      value: 40
+    }, {
+      name: 'L',
+      value: 50
+    }, {
+      name: 'XC',
+      value: 90
+    }, {
+      name: 'C',
+      value: 100
+    }, {
+      name: 'CD',
+      value: 400
+    }, {
+      name: 'D',
+      value: 500
+    }, {
+      name: 'CM',
+      value: 900
+    }, {
+      name: 'M',
+      value: 1000
+    }
+  ]
+
+  const romanLibRe = romanLib.reverse()
+  
+  let roman = ''
+
+  while(number>0) {
+    for(let x=0; x<romanLibRe.length; x++) {
+      if(number >= romanLib[x].value) {
+        roman += romanLib[x].name
+        number -= romanLib[x].value
+        break
+      }
+    }
+  }
+
+  return roman
 }
 
 // Drive code
