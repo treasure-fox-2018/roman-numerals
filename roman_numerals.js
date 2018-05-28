30e259d9aa8332eaa9b6ca54acc464c9e89496c1
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-05-28 15:16:06 +0700

    roman numerals

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,28 @@
 function to_roman (num) {
-  // your implementation code here
+  var lookup = {
+                M:1000,
+                CM:900,
+                D:500,
+                CD:400,
+                C:100,
+                XC:90,
+                L:50,
+                XL:40,
+                X:10,
+                IX:9,
+                V:5,
+                IV:4,
+                I:1
+              }
+  var roman = ''
+  var i;
+  for ( i in lookup ) {
+    while ( num >= lookup[i] ) {
+      roman += i;
+      num -= lookup[i];
+    }
+  }
+  return roman;
 }
 
 // Drive code
@@ -11,3 +34,4 @@ console.log('9     | IX       | ', to_roman(9))
 console.log('13    | XIII     | ', to_roman(13))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
 console.log('1646  | MDCXLVI  | ', to_roman(1646))
+// console.log('2134  | MMCXXXIV  | ', to_roman(2134)) // test
