4f60b96db6a909991f17f7546b9f68dd33af38e5
Odelia <odeliasusanto@gmail.com>
2018-05-28 18:21:06 +0700

    exercise roman numerals done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,17 @@
 function to_roman (num) {
-  // your implementation code here
+  var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var romans = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  var result = '';
+
+for (var i = 0; i < numbers.length; i++) {
+  while (num >= numbers[i]) {
+    if (num >= numbers[i]) {
+      num -= numbers[i];
+      result += romans[i];
+    }
+  }
+}
+return result;
 }
 
 // Drive code
