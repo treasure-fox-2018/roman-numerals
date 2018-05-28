8dadaad243ab9834ffe7d5307bd130fc28f41f8b
adrowicaksono <adrowicaksono@gmail.com>
2018-05-28 18:40:00 +0700

    roman numerals

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,6 +1,20 @@
 function to_roman (num) {
   // your implementation code here
+  let number = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+  let roman  = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+  let angka = num
+  let disp = [];
+
+while(angka > 0){
+  for(let a = 0; a < number.length; a++){
+      if(angka >= number[a]){
+        disp.push(roman[a]);
+        angka -= number[a];
+      }
+    }
 }
+  console.log(disp);
+}  
 
 // Drive code
 console.log('My totally sweet testing script for new roman\n')
