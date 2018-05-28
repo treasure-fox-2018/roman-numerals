7344094d48d24197177d6e951dee18dd9bda4f99
Wahyudi Setiaji <wahyudisetiaji@gmail.com>
2018-05-28 20:29:18 +0700

    Add roman numerals

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,7 +1,17 @@
+
 function to_roman (num) {
   // your implementation code here
+  var number = [1000, 900, 500, 400, 100, 50, 40, 10, 9, 5, 4, 1]
+  var romawi = ["M", "CM", "D", "CD", "C", "L", "XL","X", "IX", "V", "IV", "I"]
+  var strResult = ""
+  for(var i = 0; i < romawi.length; i++){
+    while (num >= number[i]) {
+      num -= number[i]
+      strResult += romawi[i]
+    }
+  }
+  return strResult
 }
-
 // Drive code
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
