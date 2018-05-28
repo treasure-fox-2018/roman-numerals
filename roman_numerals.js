45c97e1d594318d28adf787fc3979ce5c774c0a4
handipriyono <handimacbook@gmail.com>
2018-05-28 13:36:03 +0700

    add roman numerals

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,33 @@
-function to_roman (num) {
+function to_roman(num) {
   // your implementation code here
+  var obj = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+
+  let result = '';
+  for (let value in obj) {
+    while (num >= obj[value]) {
+      result += value;
+      num -= obj[value];
+    }
+  }
+
+
+  return result;
+
 }
 
 // Drive code
