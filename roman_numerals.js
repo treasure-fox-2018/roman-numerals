e7b8be49d39b6a1867db7ca9c9f16cf9e00c9849
fadhilhaka <fadhilhanri@gmail.com>
2018-05-31 07:06:47 +0700

    trim code

--- roman_numerals.js
+++ roman_numerals.js
@@ -15,7 +15,6 @@ function to_roman (num) {
     DM: 900,
     M: 1000,
   }
-  
   let keys = Object.keys(numObj)
   let hasil = ''
   for (let a = keys.length-1; a >= 0; a--) {
@@ -33,6 +32,6 @@ console.log('input | expected | actual')
 console.log('——————|——————————|———————')
 console.log('4     | IV       | ', to_roman(4))
 console.log('9     | IX       | ', to_roman(9))
-console.log('23    | XXIII     | ', to_roman(23))
+console.log('23    | XXIII    | ', to_roman(23))
 console.log('1453  | MCDLIII  | ', to_roman(1453))
 console.log('1646  | MDCXLVI  | ', to_roman(1646))
